de4c1faef6f05cc35f0f35b826565c034020f9be
urban <hello@urbanhacker.net>
2023-08-31 22:45:14 +0200

    add shortcut

--- Scripts/create_configuration.sh
+++ Scripts/create_configuration.sh
@@ -2,25 +2,31 @@
 
 source ./global.sh
 source ./utils.sh
-msg logo.txt
-p "We will create a new configuration"
-msg wallet_suggestions.txt
-echo ""
-r "Your bitcoin address:" address
-#python3 ../Tools/address_validation.py $address
-echo ""
-echo -e "  \033[33m?\033[0m Which mining pool would you like to use?"
-mining_pool=$(gum choose --cursor=  ›  public-pool.io solo.ckpool.org custom)
-p_user $mining_pool
 
-if [[ $mining_pool == "custom" ]]; then
-    r "Mining pool stratum URL:" $mining_pool
-fi
-echo ""
-p "Creating worker.sh"
-python3 ../Tools/create_worker.py $mining_pool $address > ../Worker/worker.sh
-chmod +x ../Worker/worker.sh
-echo "address: $address" > ../Worker/worker.conf
-echo "pool: $mining_pool" >> ../Worker/worker.conf
-p "Ready to boil the oceans captain!"
-echo ""
+new_configuration(){
+    msg logo.txt
+    p "We will create a new configuration"
+    msg wallet_suggestions.txt
+    echo ""
+    r "Your bitcoin address:" address
+    #python3 ../Tools/address_validation.py $address
+    echo ""
+    echo -e "  \033[33m?\033[0m Which mining pool would you like to use?"
+    mining_pool=$(gum choose --cursor=  ›  public-pool.io solo.ckpool.org custom)
+    p_user $mining_pool
+
+    if [[ $mining_pool == "custom" ]]; then
+        r "Mining pool stratum URL:" $mining_pool
+    fi
+    echo ""
+    p "Creating worker.sh"
+    python3 ../Tools/create_worker.py $mining_pool $address > ../Worker/worker.sh
+    chmod +x ../Worker/worker.sh
+    echo "address: $address" > ../Worker/worker.conf
+    echo "pool: $mining_pool" >> ../Worker/worker.conf
+    p "Ready to boil the oceans captain!"
+    p_ok "To start mining, use the command 'shuriken start'"
+    echo ""
+}
+
+new_configuration
---
+++ shuriken.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+command=$1
+shift
+cd ./Scripts
+
+if [[ $command == "stop" ]]; then
+    ./stop.sh $@
+fi
+if [[ $command == "start" ]]; then
+    ./start.sh $@
+fi
+if [[ $command == "run" ]]; then
+    ./start.sh $@
+fi
+if [[ $command == "status" ]]; then
+    ./status.sh $@
+fi
+if [[ $command == "config" ]]; then
+    ./create_configuration.sh $@
+fi
