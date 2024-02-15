9ecc51078f9491153cfca9401efbe3e53deab5a9
urban <urban@example.com>
2024-02-15 23:05:04 +0100

    forgot to properly use the working director :(. My bad

--- shuriken.sh
+++ shuriken.sh
@@ -2,29 +2,32 @@
 
 command=$1
 shift
-cd ./Scripts
 
-if [[ $command == "stop" ]]; then
-    ./stop.sh $@
-elif [[ $command == "start" ]]; then
-    ./start.sh $@
-elif [[ $command == "run" ]]; then
-    ./start.sh $@
-elif [[ $command == "status" ]]; then
-    ./status.sh $@
-elif [[ $command == "config" ]]; then
-    ./create_configuration.sh $@
-elif [[ $command == "monitor" ]]; then
-    ./monitor.sh $@
-elif [[ $command == "miner" ]]; then
-    ./monitor.sh $@
-elif [[ $command == "test" ]]; then
-    ./selftest.sh $@
-elif [[ $command == "help" ]]; then
-    ./help.sh $@
-elif [[ $command == "version" ]]; then
-    ./version.sh $@
-else
-    echo "  » Unknown command. Not sure what you are trying to do :("
-    echo "    Run 'shuriken help' to show the built-in user manual"
-fi
+{
+    cd ~/shurikenpi/shurikenpi.io/Scripts
+
+    if [[ $command == "stop" ]]; then
+        ./stop.sh $@
+    elif [[ $command == "start" ]]; then
+        ./start.sh $@
+    elif [[ $command == "run" ]]; then
+        ./start.sh $@
+    elif [[ $command == "status" ]]; then
+        ./status.sh $@
+    elif [[ $command == "config" ]]; then
+        ./create_configuration.sh $@
+    elif [[ $command == "monitor" ]]; then
+        ./monitor.sh $@
+    elif [[ $command == "miner" ]]; then
+        ./monitor.sh $@
+    elif [[ $command == "test" ]]; then
+        ./selftest.sh $@
+    elif [[ $command == "help" ]]; then
+        ./help.sh $@
+    elif [[ $command == "version" ]]; then
+        ./version.sh $@
+    else
+        echo "  » Unknown command. Not sure what you are trying to do :("
+        echo "  » Run 'shuriken help' to show the built-in user manual"
+    fi
+}
