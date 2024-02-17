3f5e2405747ca5d05dd86438b1080094dccbe18f
urban <urban@example.com>
2024-02-17 22:30:34 +0100

    Detect if not in git repository

--- Scripts/global.sh
+++ Scripts/global.sh
@@ -15,7 +15,7 @@ enum ()
     done
 }
 
-enum VERSION_TYPE { STABLE, DEV, DEV_DETACHED };
+enum VERSION_TYPE { STABLE, DEV, DEV_DETACHED, NO_GIT };
 
 VERBOSE=0
 while getopts ":v" option; do
@@ -42,16 +42,25 @@ mkdir $TMP_DIRECTORY 2>&1
 
 GIT_URL=https://github.com/Urban-Hacker/shurikenpi.io/
 
-SESSION_COUNT=$(screen -ls | grep -c "\.shurikenpi.io")
+if command -v screen >/dev/null 2>&1; then
+    SESSION_COUNT=$(screen -ls | grep -c "\.shurikenpi.io")
+else
+    SESSION_COUNT=0
+fi
 
 extract_version(){
+    INSTALLED_VERSION=""
+    INSTALLED_TYPE=""
+    if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
+        INSTALLED_VERSION="NO GIT"
+        INSTALLED_TYPE=$NO_GIT
+        return 0
+    fi
     local branch=$(git rev-parse --abbrev-ref HEAD)
     local tag=$(git describe --tags --abbrev=0 2>/dev/null)
     local commit_hash=$(git rev-parse HEAD)
     local commit_hash=${commit_hash:0:5}
 
-    INSTALLED_VERSION=""
-    INSTALLED_TYPE=""
     if [[ "$branch" == "HEAD" ]]; then
         INSTALLED_VERSION="$commit_hash (dev)"
         INSTALLED_TYPE=$DEV_DETACHED
